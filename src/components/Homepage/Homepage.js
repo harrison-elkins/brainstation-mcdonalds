d282d84254fad6262466d074e1bb677b27a7e456
aymeeflores <aymeeflores@users.noreply.github.com>
2021-08-12 23:56:57 -0400

    homepage sketch

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Homepage extends React.Component {
+  render() {
+    return;
+    <>
+      <section>
+        <h2>New!</h2>
+        <h1>McDonalds Bingo</h1>
+        <h3>Earn free rewards when you purchase menu items</h3>
+        <button>View Your Cards</button>
+      </section>
+      <section>
+        <div>
+          For a limited time only. At participating McDonald’s restaurants in
+          Canada
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Menu</h2>
+          <button>Full Menu</button>
+        </div>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+      </section>
+      <section>
+        <h2>Scan and Earn</h2>
+        <div>Rewards</div>
+      </section>
+      <footer>
+        <navbar></navbar>
+      </footer>
+    </>;
+  }
+}

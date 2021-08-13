b93627ce446908f1eee066fe6e12bd6bbacb0d43
aymeeflores <aymeeflores@users.noreply.github.com>
2021-08-13 12:23:37 -0400

    component styling done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Bingo from "./pages/Bingo/Bingo";
+import Addtobag from "./pages/Add-to-bag/Add-to-bag";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />

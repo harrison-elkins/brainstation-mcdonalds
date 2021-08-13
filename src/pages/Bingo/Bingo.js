13a206da4dd2e1871ca7c45940443914192885d4
Harrison Elkins <harrisonmelkins@gmail.com>
2021-08-12 23:14:45 -0400

    structure folder

---
+++ src/pages/Bingo/Bingo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Bingo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bingo!</h1>
+      </div>
+    );
+  }
+}
